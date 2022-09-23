d0182d42fe70b7d5d7df8bb4c1609b61efe1940f
Samuel Akinosho <39565075+LucidSamuel@users.noreply.github.com>
2022-09-23 19:25:40 +0400

    detecting errors

--- example.js
+++ example.js
@@ -4,9 +4,13 @@ var lucidmath = require('./lucidmath.js');
 var expr = lucidmath.compile("100+5*2");
 console.log(expr()); //110
 
-
 //working external function
 var triple = function(s) {s.push(s.pop()*3);};
 var funcs = {trip: [1, triple]};
 expr = lucidmath.compile("trip(100)", funcs);
 console.log(expr()); //300
+
+//Detecting error (unbalanced param)
+expr = lucidmath.compile("1+(4*2");
+console.log(expr); //{text: '1+(4*2', index: 6, token: '', type: 'paren'}
+
