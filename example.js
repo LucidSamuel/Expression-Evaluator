289dd357e329078f919132097a37a79e1d1bf92a
Samuel Akinosho <39565075+LucidSamuel@users.noreply.github.com>
2022-09-23 19:23:46 +0400

    example update

--- example.js
+++ example.js
@@ -1,1 +1,12 @@
-var rimath = require('./lucidmath.js');
+var lucidmath = require('./lucidmath.js');
+
+//Simple usages
+var expr = lucidmath.compile("100+5*2");
+console.log(expr()); //110
+
+
+//working external function
+var triple = function(s) {s.push(s.pop()*3);};
+var funcs = {trip: [1, triple]};
+expr = lucidmath.compile("trip(100)", funcs);
+console.log(expr()); //300
